f1ec4b60a751b8066493b95a59ea4572b2b4c9f0
Andrei Negriu <81017870+NegriuAndrei@users.noreply.github.com>
2025-04-05 12:34:58 +0300

    adaugare masina de politie

--- gta/assets/scripts/map.gd
+++ gta/assets/scripts/map.gd
@@ -12,14 +12,14 @@ func _ready():
 		car.connect("entered_by_player", Callable(self, "_on_car_entered_by_player"))
 
 func _on_car_entered_by_player(car):
-	# Dezactivează playerul
-	player.get_node("sprite").visible = true
-	player.get_node("Collision").disabled = true
-	player.is_current_character_active = false
+	# Dezactivează playerul (deferred)
+	player.get_node("sprite").set_deferred("visible", false)
+	player.get_node("Collision").set_deferred("disabled", true)
+	player.set_deferred("is_current_character_active", false)
 
-	# Activează mașina
-	car.visible = true
-	car.get_node("CollisionShape2D").disabled = false
-	car.is_current_character_active = true
+	# Activează mașina (deferred)
+	car.set_deferred("visible", true)
+	car.get_node("CollisionShape2D").set_deferred("disabled", false)
+	car.set_deferred("is_current_character_active", true)
 
 	is_current_character_active = false
